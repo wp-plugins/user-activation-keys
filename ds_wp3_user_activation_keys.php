6f978f1727c892787e41c3f6c578fc4cd728aaa4
dsader <dsader@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-26 12:18:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/user-activation-keys/trunk@281070 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ds_wp3_user_activation_keys.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: User Activation Keys
+Plugin URI: http://dsader.snowotherway.org/wordpress-plugins/user-activation-keys/
+Description: WP Network Multisite user activation key removal or approval "mu=plugin". See SuperAdmin->"User Activation Keys" to delete activation keys - to allow immediate (re)signup of users who otherwise get the "try again in two days" message. Also, users waiting to be activated (or can't because the email with the generated activation link is "gone") can be approved manually.
+Author: D. Sader
+Version: 3.0.1
+Author URI: http://dsader.snowotherway.org
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+*/
+
+add_action('admin_menu', 'ds_uak_admin_page');
+
+function ds_uak_admin_page() {
+      if (is_super_admin()) {
+        add_submenu_page('ms-admin.php', 'User Activation Keys', 'User Activation Keys', 10, 'act_keys', 'ds_delete_stale');
+      }
+}
+
+function ds_delete_stale() {
+	global $wpdb;
+ 	$query = "SELECT * FROM {$wpdb->signups} ORDER BY registered DESC";
+	$results = $wpdb->get_results($query, ARRAY_A);
+	$delete = $_GET['delete'];
+	$del_stale_active = $_GET['del_stale_active'];
+	$del_stale_inactive = $_GET['del_stale_inactive'];
+	$location = admin_url('ms-admin.php?page=act_keys');
+
+
+	if ( $delete ) {
+		check_admin_referer('activation_key');
+		$wpdb->query("DELETE FROM $wpdb->signups WHERE activation_key = '$delete'");
+           	echo "<meta http-equiv='refresh' content='0;url=$location' />";
+            exit;
+	}
+	if ( $del_stale_active ) {
+		check_admin_referer('activation_key');
+	        $wpdb->query( "DELETE FROM $wpdb->signups WHERE active = 1 AND DATE(registered) < DATE_SUB(curdate(), INTERVAL 30 DAY)" );
+           	echo "<meta http-equiv='refresh' content='0;url=$location' />";
+            exit;
+	}
+	if ( $del_stale_inactive ) {
+		check_admin_referer('activation_key');
+	        $wpdb->query( "DELETE FROM $wpdb->signups WHERE active = 0 AND DATE(registered) < DATE_SUB(curdate(), INTERVAL 30 DAY)" );
+           	echo "<meta http-equiv='refresh' content='0;url=$location' />";
+            exit;
+	}
+	
+	echo '<div class="wrap">';
+		echo "<h2>User Activation Keys</h2>";
+	if ( $results ) {
+		echo '<p>The following is a list of user activation keys from $wpdb->signups. Delete a key to allow the username to (re)signup and bypass the "couple days" it takes WP to free up its hold on a user name. You can also manually approve users that for whatever reason have not completed their activation.</p>';
+		echo '<div class="tablenav"> <span class="alignleft">';
+		echo '<a class="button-secondary" href="' . wp_nonce_url( $location . '&del_stale_active', 'activation_key' ) . '" class="delete">'.__('Delete stale active signup keys older than 30 days').'</a>';
+		echo '<a class="button-secondary" href="' . wp_nonce_url( $location . '&del_stale_inactive', 'activation_key' ) . '" class="delete">'.__('Delete stale inactive signup keys older than 30 days').'</a>';
+		echo '</span>';
+		echo '</div><br class="clear" />';
+		echo '<table class="widefat"><tbody>';
+		echo '<thead><th>#</th><th>Registered</th><th>User</th><th>Email</th><th>Action</th></thead>';
+		foreach ( $results as $rows ) {
+			echo '<tr><td>' . ++$ct . '</td><td>'.$rows['registered'].'</td><td>'.$rows['user_login'].'</td><td>'.$rows['user_email'].'</td>';
+			if($rows['active'] != '1') {
+			echo '<td><a href="' . admin_url() . 'wp-activate.php?key='.$rows['activation_key'] . '" target="_blank">approve</a> | <a href="' . wp_nonce_url( $location . '&delete='.$rows['activation_key'], 'activation_key' ) . '">delete unused key</a></td>';
+			} else {
+			echo '<td>User Activated '.$rows['activated'].' | <a href="' .  wp_nonce_url( $location . '&delete='.$rows['activation_key'] , 'activation_key' ).'">delete uncecessary key</a></td>';
+			}
+			echo '</tr>';
+		}
+		echo '</tbody></table>';
+	} else {
+		echo '<p>No user activation keys in $wpdb->signups. If you delete a user, you should be able to reuse the username immediatley. If the user still had a registration key, it would need to be deleted before you could signup again right away with the same username. You can also manually approve users that for whatever reason have not completed their activation.</p>';
+	}
+	echo '</div>';
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== User Activation Keys ===
+Contributors: dsader
+Donate link: http://dsader.snowotherway.org
+Tags: signup, username, activation, network, network user activation, activation keys, multisite, network, users
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: Trunk
+
+WP Network Multisite "mu=plugin" for user activation key removal or approval. 
+
+== Description ==
+
+Ever signup a user account, delete it, and try to signup up the same username again? 
+
+Ever try to support a new user who created a username, but entered the wrong email address and so can't activate, and can't signup with the same username?
+
+Ever wanted to signup a bunch of users with phony emails so users without email could start blogging right away?
+
+I have, so I made a plugin to help me.
+
+WP Network Multisite "mu=plugin" for user activation key removal or approval. See SuperAdmin-->"User Activation Keys" to delete activation keys - to allow immediate (re)signup of users who otherwise get the "try again in two days" message. Also, users waiting to be activated (or can't because the email with the generated activation link is "gone") can be approved manually.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `ds_wp3_user_activation_keys.php` to the `/wp-content/mu-plugins/` directory
+2. Delete or approve keys at SuperAdmin->"User Activation Keys" page
+
+== Frequently Asked Questions ==
+
+* I deleted an activation key, but I still can't signup with that username? Delete the user from SuperAdmin->Users and delete the activation key.
+* Why are there buttons to mass delete keys over 30 days old? I needed the buttons when upgrading an ancient WPMU install several years ago. I left them in the plugin even though my sites/networks can no longer reproduce the accumulation of keys that makes them useful.
+* Is it safe to delete keys from otherwise active users? Yes, as far as I know. I don't know why the activation key for activated users remains behind. But they appear to delete autoomagicaly after "a couple of days" anyway.
+* Some users have an activation key in $wpdb->users? That is normal. This plugin doesn't touch those keys, only the ones in $wpdb->signup.
+
+== Screenshots ==
+
+1. SuperAdmin->"User Activation Keys" page
+
+== Changelog ==
+= 3.0.1 = 
+* initial release
+
+== Upgrade Notice ==
+= 3.0.1 =
+* initial release
